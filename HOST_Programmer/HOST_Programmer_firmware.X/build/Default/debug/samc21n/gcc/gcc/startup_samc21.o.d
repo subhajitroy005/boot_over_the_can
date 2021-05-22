4bead6e007c75b82fe7a341e767825fbba6b68f0
subhajitroy005 <subhajitroy005@gmail.com>
2021-05-22 17:52:00 +0530

    :recycle: Folder structure added: driver , build support.

---
+++ HOST_Programmer/HOST_Programmer_firmware.X/build/Default/debug/samc21n/gcc/gcc/startup_samc21.o.d
@@ -0,0 +1,222 @@
+build/Default/debug/samc21n/gcc/gcc/startup_samc21.o: \
+ samc21n/gcc/gcc/startup_samc21.c samc21n/include/samc21.h \
+ samc21n/include/samc21n18a.h CMSIS/Core/Include/core_cm0plus.h \
+ CMSIS/Core/Include/cmsis_version.h CMSIS/Core/Include/cmsis_compiler.h \
+ CMSIS/Core/Include/cmsis_gcc.h CMSIS/Core/Include/mpu_armv7.h \
+ samc21n/include/system_samc21.h samc21n/include/component/ac.h \
+ samc21n/include/component/adc.h samc21n/include/component/can.h \
+ samc21n/include/component/ccl.h samc21n/include/component/dac.h \
+ samc21n/include/component/divas.h samc21n/include/component/dmac.h \
+ samc21n/include/component/dsu.h samc21n/include/component/eic.h \
+ samc21n/include/component/evsys.h samc21n/include/component/freqm.h \
+ samc21n/include/component/gclk.h samc21n/include/component/hmatrixb.h \
+ samc21n/include/component/mclk.h samc21n/include/component/mtb.h \
+ samc21n/include/component/nvmctrl.h samc21n/include/component/oscctrl.h \
+ samc21n/include/component/osc32kctrl.h samc21n/include/component/pac.h \
+ samc21n/include/component/pm.h samc21n/include/component/port.h \
+ samc21n/include/component/rstc.h samc21n/include/component/rtc.h \
+ samc21n/include/component/sdadc.h samc21n/include/component/sercom.h \
+ samc21n/include/component/supc.h samc21n/include/component/tc.h \
+ samc21n/include/component/tcc.h samc21n/include/component/tsens.h \
+ samc21n/include/component/wdt.h samc21n/include/instance/ac.h \
+ samc21n/include/instance/adc0.h samc21n/include/instance/adc1.h \
+ samc21n/include/instance/can0.h samc21n/include/instance/can1.h \
+ samc21n/include/instance/ccl.h samc21n/include/instance/dac.h \
+ samc21n/include/instance/divas.h samc21n/include/instance/dmac.h \
+ samc21n/include/instance/dsu.h samc21n/include/instance/eic.h \
+ samc21n/include/instance/evsys.h samc21n/include/instance/freqm.h \
+ samc21n/include/instance/gclk.h samc21n/include/instance/hmatrixhs.h \
+ samc21n/include/instance/mclk.h samc21n/include/instance/mtb.h \
+ samc21n/include/instance/nvmctrl.h samc21n/include/instance/oscctrl.h \
+ samc21n/include/instance/osc32kctrl.h samc21n/include/instance/pac.h \
+ samc21n/include/instance/pm.h samc21n/include/instance/port.h \
+ samc21n/include/instance/ptc.h samc21n/include/instance/rstc.h \
+ samc21n/include/instance/rtc.h samc21n/include/instance/sdadc.h \
+ samc21n/include/instance/sercom0.h samc21n/include/instance/sercom1.h \
+ samc21n/include/instance/sercom2.h samc21n/include/instance/sercom3.h \
+ samc21n/include/instance/sercom4.h samc21n/include/instance/sercom5.h \
+ samc21n/include/instance/sercom6.h samc21n/include/instance/sercom7.h \
+ samc21n/include/instance/supc.h samc21n/include/instance/tc0.h \
+ samc21n/include/instance/tc1.h samc21n/include/instance/tc2.h \
+ samc21n/include/instance/tc3.h samc21n/include/instance/tc4.h \
+ samc21n/include/instance/tc5.h samc21n/include/instance/tc6.h \
+ samc21n/include/instance/tc7.h samc21n/include/instance/tcc0.h \
+ samc21n/include/instance/tcc1.h samc21n/include/instance/tcc2.h \
+ samc21n/include/instance/tsens.h samc21n/include/instance/wdt.h \
+ samc21n/include/pio/samc21n18a.h
+
+samc21n/include/samc21.h:
+
+samc21n/include/samc21n18a.h:
+
+CMSIS/Core/Include/core_cm0plus.h:
+
+CMSIS/Core/Include/cmsis_version.h:
+
+CMSIS/Core/Include/cmsis_compiler.h:
+
+CMSIS/Core/Include/cmsis_gcc.h:
+
+CMSIS/Core/Include/mpu_armv7.h:
+
+samc21n/include/system_samc21.h:
+
+samc21n/include/component/ac.h:
+
+samc21n/include/component/adc.h:
+
+samc21n/include/component/can.h:
+
+samc21n/include/component/ccl.h:
+
+samc21n/include/component/dac.h:
+
+samc21n/include/component/divas.h:
+
+samc21n/include/component/dmac.h:
+
+samc21n/include/component/dsu.h:
+
+samc21n/include/component/eic.h:
+
+samc21n/include/component/evsys.h:
+
+samc21n/include/component/freqm.h:
+
+samc21n/include/component/gclk.h:
+
+samc21n/include/component/hmatrixb.h:
+
+samc21n/include/component/mclk.h:
+
+samc21n/include/component/mtb.h:
+
+samc21n/include/component/nvmctrl.h:
+
+samc21n/include/component/oscctrl.h:
+
+samc21n/include/component/osc32kctrl.h:
+
+samc21n/include/component/pac.h:
+
+samc21n/include/component/pm.h:
+
+samc21n/include/component/port.h:
+
+samc21n/include/component/rstc.h:
+
+samc21n/include/component/rtc.h:
+
+samc21n/include/component/sdadc.h:
+
+samc21n/include/component/sercom.h:
+
+samc21n/include/component/supc.h:
+
+samc21n/include/component/tc.h:
+
+samc21n/include/component/tcc.h:
+
+samc21n/include/component/tsens.h:
+
+samc21n/include/component/wdt.h:
+
+samc21n/include/instance/ac.h:
+
+samc21n/include/instance/adc0.h:
+
+samc21n/include/instance/adc1.h:
+
+samc21n/include/instance/can0.h:
+
+samc21n/include/instance/can1.h:
+
+samc21n/include/instance/ccl.h:
+
+samc21n/include/instance/dac.h:
+
+samc21n/include/instance/divas.h:
+
+samc21n/include/instance/dmac.h:
+
+samc21n/include/instance/dsu.h:
+
+samc21n/include/instance/eic.h:
+
+samc21n/include/instance/evsys.h:
+
+samc21n/include/instance/freqm.h:
+
+samc21n/include/instance/gclk.h:
+
+samc21n/include/instance/hmatrixhs.h:
+
+samc21n/include/instance/mclk.h:
+
+samc21n/include/instance/mtb.h:
+
+samc21n/include/instance/nvmctrl.h:
+
+samc21n/include/instance/oscctrl.h:
+
+samc21n/include/instance/osc32kctrl.h:
+
+samc21n/include/instance/pac.h:
+
+samc21n/include/instance/pm.h:
+
+samc21n/include/instance/port.h:
+
+samc21n/include/instance/ptc.h:
+
+samc21n/include/instance/rstc.h:
+
+samc21n/include/instance/rtc.h:
+
+samc21n/include/instance/sdadc.h:
+
+samc21n/include/instance/sercom0.h:
+
+samc21n/include/instance/sercom1.h:
+
+samc21n/include/instance/sercom2.h:
+
+samc21n/include/instance/sercom3.h:
+
+samc21n/include/instance/sercom4.h:
+
+samc21n/include/instance/sercom5.h:
+
+samc21n/include/instance/sercom6.h:
+
+samc21n/include/instance/sercom7.h:
+
+samc21n/include/instance/supc.h:
+
+samc21n/include/instance/tc0.h:
+
+samc21n/include/instance/tc1.h:
+
+samc21n/include/instance/tc2.h:
+
+samc21n/include/instance/tc3.h:
+
+samc21n/include/instance/tc4.h:
+
+samc21n/include/instance/tc5.h:
+
+samc21n/include/instance/tc6.h:
+
+samc21n/include/instance/tc7.h:
+
+samc21n/include/instance/tcc0.h:
+
+samc21n/include/instance/tcc1.h:
+
+samc21n/include/instance/tcc2.h:
+
+samc21n/include/instance/tsens.h:
+
+samc21n/include/instance/wdt.h:
+
+samc21n/include/pio/samc21n18a.h:
